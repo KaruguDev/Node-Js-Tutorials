833a1588e0fd01f4b22138f9c9ec131c97d6501a
paul_100 <paul.k.karugu@gmail.com>
2018-12-05 16:56:29 +0300

    updated config.js file

--- node-todo-api/server/config/config.js
+++ node-todo-api/server/config/config.js
@@ -1,9 +1,17 @@
 //SET ENV
 var env = process.env.NODE_ENV || 'development'
-//console.log(env)
 
-if(env === 'development'){
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/ToDoApp'
-}else if(env === 'test'){
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/ToDoAppTest'
+if(env === "development" || env === 'test'){
+  var config = require('./config.json')
+  var envConfig = config[env]
+
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key]
+  })
 }
+
+// if(env === 'development'){
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/ToDoApp'
+// }else if(env === 'test'){
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/ToDoAppTest'
+// }
