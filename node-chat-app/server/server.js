15d891db22e7b5b126cb72c238a7bb86a1387e37
paul_100 <paul.k.karugu@gmail.com>
2018-12-07 18:49:04 +0300

    templates design

--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -1,22 +1,50 @@
 const path = require('path')
 const http = require('http')
+const hbs = require('hbs')
 const express = require('express')
 const socketIO = require('socket.io')
 
 const publicPath = path.join(__dirname, '..', 'public')
+const partialsPath = path.join(__dirname, '../views/partials')
 const config = path.join(__dirname, '..', 'config', 'config.js')
 
 //enviroment configurations
 require(config)
 
 var app = express()
-//static files
+
+//register partials
+hbs.registerPartials(partialsPath)
+app.set('view engine', 'hbs')
+
+//helpers
+hbs.registerHelper('public', () => {
+  return publicPath
+})
+
+//public files
 app.use(express.static(publicPath))
+
+//routes
+app.get('/', (req, resp) => {
+  resp.render('home.hbs', {
+
+  })
+
+})
+
+app.get('/chat', (req, resp) => {
+  resp.render('chatroom.hbs', {
+
+  })
+
+})
 
 //http server with sockets
 var httpServer = http.createServer(app)
 var io = socketIO(httpServer)
 
+//sockets
 io.on('connection', (socket) => {
   console.log('there is a new user connected')
 
